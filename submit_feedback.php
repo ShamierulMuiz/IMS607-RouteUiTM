259727f353bd5e6ce982f801e7e8e02374ce3147
SHAMIERUL MUIZ <97096323+ShamierulMuiz@users.noreply.github.com>
2024-02-06 18:57:49 +0800

    Update submit_feedback.php

--- submit_feedback.php
+++ submit_feedback.php
@@ -1,20 +1,15 @@
 <?php
-// Include your database connection file here
 include("home.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve the message from the form
     $message = mysqli_real_escape_string($conn, $_POST['message_feedback']);
 
-    // Insert the message into the database
     $query = "INSERT INTO messages (message) VALUES ('$message')";
     $result = mysqli_query($conn, $query);
 
     if ($result) {
-        // Success! You can redirect the user or display a success message.
         echo "Your message has been successfully submitted!";
     } else {
-        // Handle the error if the insertion fails
         echo "Error: " . mysqli_error($conn);
     }
 }
